1b14417b99a259df01f1d863e9634be307eee0e9
Bogdan Weimer <weimer.bogdan@icloud.com>
2024-05-17 13:47:01 +0200

    ✨ adds bentogrid

---
+++ components/BentoTile.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+defineProps({
+  size: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<template>
+
+  <div :data-bento="size" class="bento">
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.bento {
+  background-color: hsl(0, 0%, 96%);
+  border: 2px solid hsl(0, 0%, 92%);
+  border-radius: 24px;
+
+  &>.content {
+    height: 100%;
+    padding: 15px;
+  }
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="page">
+    <BentoGrid>
+      <BentoTile size="2x2">
+        <h2>2x2</h2>
+      </BentoTile>
+      <BentoTile size="1x1">
+        <h2>1x1</h2>
+      </BentoTile>
+      <BentoTile size="2x2">
+        <h2>2x2</h2>
+      </BentoTile>
+      <BentoTile size="2x1">
+        <h2>2x1</h2>
+      </BentoTile>
+      <BentoTile size="1x1">
+        <h2>1x1</h2>
+      </BentoTile>
+      <BentoTile size="1x2">
+        <h2>1x2</h2>
+      </BentoTile>
+      <BentoTile size="2x2">
+        <h2>2x2</h2>
+      </BentoTile>
+      <BentoTile size="2x2">
+        <h2>2x2</h2>
+      </BentoTile>
+      <BentoTile size="1x3">
+        <h2>1x3</h2>
+      </BentoTile>
+    </BentoGrid>
+  </div>
+</template>
+
+<style lang="scss">
+.page {
+  max-width: 1024px;
+  margin-inline: auto;
+  padding: 18px;
+}
+</style>
